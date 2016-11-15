abca9788e9e409e6435191f92e51ab5edf6990cb
Justin Cox <dubstepandponies95@gmail.com>
2016-11-15 09:38:23 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
-console.log('This would be the main JS file.');
+(function ($, undefined) {
+  // repo URLs
+  var repoUrls = {
+    "welt": "http://bongos.studio/welt",
+    "bongosstudio.github.io": "http://bongos.studio"
+  };
+  
+  function repoUrl(repo) {
+    return repoUrls[repo.Name] || repo.html_url;
+  }
+  
+  // repo descriptions
+  var repoDescs = {
+    "welt": "Hello Universe. A game built in C# using the MonoGame Framework based on space exploration."
+  }
+  
+  function repoDesc(repo) {
+    return repoDescs[repo.Name] || repo.description;
+  }
+})
